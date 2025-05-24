e920b847cf95dd026eb640db085ca79e44f5ef1a
Bence Gombkötő <kucukasd@gmail.com>
2025-05-24 10:53:05 +0200

    get html

--- scraper.py
+++ scraper.py
@@ -2,3 +2,11 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+
+url = "https://www.worldometers.info/geography/flags-of-the-world/"
+headers = {"User-Agent": "Mozilla/5.0"}
+
+os.makedirs("images", exist_ok=True)
+
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, "html.parser")
