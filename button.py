db1a21b6f767cae43809a333b7982eccdaded74a
Bence Gombkötő <kucukasd@gmail.com>
2025-05-27 07:25:00 +0200

    add typehint to button

--- button.py
+++ button.py
@@ -2,22 +2,20 @@ import pygame
 
 
 class Button:
-    def __init__(
-        self, text, x, y, width, height, color=(180, 180, 180), text_color=(0, 0, 0)
-    ):
+    def __init__(self, text: str, x: int, y: int, width: int, height: int):
         self.rect = pygame.Rect(x, y, width, height)
-        self.color = color
         self.text = text
-        self.text_color = text_color
+        self.color = (180, 180, 180)
+        self.text_color = (0, 0, 0)
         self.font = pygame.font.Font(None, 32)
 
-    def draw(self, screen):
+    def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.rect(screen, self.color, self.rect)
         text_surface = self.font.render(self.text, True, self.text_color)
         text_rect = text_surface.get_rect(center=self.rect.center)
         screen.blit(text_surface, text_rect)
 
-    def is_clicked(self, event):
+    def is_clicked(self, event: pygame.event.EventType) -> bool:
         return event.type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(
             event.pos
         )
