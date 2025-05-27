c4cd2fdeac9e4ded6e50d6e1b337c2fc308a332a
Bence Gombkötő <kucukasd@gmail.com>
2025-05-27 07:20:36 +0200

    add typehint to answer label

--- answer_label.py
+++ answer_label.py
@@ -6,14 +6,14 @@ class AnswerLabel:
         self.font = pygame.font.Font(None, 64)
         self.revealed = False
 
-    def set_answer(self, answer: str):
+    def set_answer(self, answer: str) -> None:
         self.revealed = False
         self.answer_text = answer.title()
 
-    def reveal(self):
+    def reveal(self) -> None:
         self.revealed = True
 
-    def draw(self, screen, x, y):
+    def draw(self, screen: pygame.Surface, x: int, y: int) -> None:
         text = self.answer_text if self.revealed else "???"
         text_surface = self.font.render(text, True, (0, 0, 0))
         text_rect = text_surface.get_rect(center=(x, y))
