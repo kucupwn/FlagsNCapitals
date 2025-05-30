7eb0fedf5eb1cbe8dd201fb82cf06c4779d0a69b
Bence Gombkötő <kucukasd@gmail.com>
2025-05-30 16:14:48 +0200

    add type hints to Counter

--- ui_components/counter.py
+++ ui_components/counter.py
@@ -2,12 +2,12 @@ import pygame
 
 
 class Counter:
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int) -> None:
         self.font = pygame.font.Font(None, 48)
         self.x = x
         self.y = y
 
-    def draw(self, screen, current_count, max_count):
+    def draw(self, screen: pygame.Surface, current_count: int, max_count: int) -> None:
         text = f"{current_count} / {max_count}"
         text_surface = self.font.render(text, True, (0, 0, 0))
         text_rect = text_surface.get_rect(center=(self.x, self.y))
