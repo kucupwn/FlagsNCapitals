509ea84ff597cabea8527e50c20616e19f5b4878
Bence Gombkötő <kucukasd@gmail.com>
2025-05-30 16:17:35 +0200

    add docstrings to answer label

--- ui_components/answer_label.py
+++ ui_components/answer_label.py
@@ -2,18 +2,32 @@ import pygame
 
 
 class AnswerLabel:
-    def __init__(self):
+    def __init__(self) -> None:
         self.font = pygame.font.Font(None, 64)
         self.revealed = False
 
     def set_answer(self, answer: str) -> None:
+        """
+        Sets current answer
+        Converts it to title case
+        """
+
         self.revealed = False
         self.answer_text = answer.title()
 
     def reveal(self) -> None:
+        """
+        Toggles answer displaying
+        """
+
         self.revealed = True
 
     def draw(self, screen: pygame.Surface, x: int, y: int) -> None:
+        """
+        Sets text to unknown or answer
+        Draws on screens
+        """
+
         text = self.answer_text if self.revealed else "???"
         text_surface = self.font.render(text, True, (0, 0, 0))
         text_rect = text_surface.get_rect(center=(x, y))
